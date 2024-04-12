4b092bdeab54357e1a5f4054a4f87aae1423b052
gabrielmedsilva <gabrielmedsilva@outlook.com>
2024-04-12 02:24:39 -0300

    fix: Altera estrutura do app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FiltroComponent } from './components/filtro/filtro.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, HeaderComponent, FiltroComponent],
 })
-export class AppComponent {
-  title = 'frontend';
-}
+export class AppComponent {}
