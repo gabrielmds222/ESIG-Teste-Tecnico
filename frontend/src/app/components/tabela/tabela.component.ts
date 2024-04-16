94b5efcba9d74d87193c80cdb2bfddf0df707efd
gabrielmedsilva <gabrielmedsilva@outlook.com>
2024-04-16 00:00:40 -0300

    feat: Adiciona listagem das tarefas e botão de delete

--- frontend/src/app/components/tabela/tabela.component.ts
+++ frontend/src/app/components/tabela/tabela.component.ts
@@ -1,37 +1,47 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tabela',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './tabela.component.html',
   styleUrl: './tabela.component.scss',
 })
 export class TabelaComponent {
-  items = [
-    {
-      numero: 1,
-      titulo: 'Estudo',
-      responsavel: 'João',
-      deadline: '16/04/2024',
-    },
-    {
-      numero: 2,
-      titulo: 'Trabalho',
-      responsavel: 'João',
-      deadline: '16/04/2024',
-    },
-    {
-      numero: 3,
-      titulo: 'N tenho ideia',
-      responsavel: 'João',
-      deadline: '16/04/2024',
-    },
-  ];
+  items: any[] = [];
 
-  excluirItem(item: any) {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.listarTarefas();
+  }
+
+  listarTarefas() {
+    this.http.get<any[]>('http://localhost:8080/tarefas').subscribe(
+      (response) => {
+        this.items = response;
+      },
+      (error) => {
+        console.error('Erro ao obter lista de tarefas:', error);
+      }
+    );
+  }
+
+  excluirTarefa(item: any) {
     console.log('Item excluído:', item);
+    this.http.delete(`http://localhost:8080/tarefas/${item.id}`).subscribe(
+      (response) => {
+        console.log('Tarefa excluída com sucesso.');
+        // Recarrega a lista após a exclusão bem-sucedida
+        this.listarTarefas();
+      },
+      (error) => {
+        console.error('Erro ao excluir tarefa:', error);
+      }
+    );
   }
 
   editarItem(item: any) {
