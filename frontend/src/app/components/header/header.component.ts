9d40bfa6d95d28dc0f46324c7be4d7e4f97a8226
gabrielmedsilva <gabrielmedsilva@outlook.com>
2024-04-15 23:29:45 -0300

    feat: Envia os dados do formulário direto para o banco

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,10 +1,18 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, TemplateRef, ViewChild } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, HttpClientModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
@@ -18,5 +26,38 @@ export class HeaderComponent {
 
   closeModal() {
     this.showModal = false;
+  }
+
+  formGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
+
+  ngOnInit() {
+    this.formGroup = this.formBuilder.group({
+      titulo: ['', Validators.required],
+      descricao: [''],
+      responsavel: ['', Validators.required],
+      deadline: ['', Validators.required],
+      prioridade: ['', Validators.required],
+    });
+  }
+
+  submitForm() {
+    if (this.formGroup.valid) {
+      console.log('Formulário enviado:', this.formGroup.value);
+      this.http
+        .post('http://localhost:8080/tarefas', this.formGroup.value)
+        .subscribe(
+          (response) => {
+            console.log('Resposta do servidor:', response);
+            // Você pode adicionar lógica adicional aqui, se necessário
+          },
+          (error) => {
+            console.error('Erro ao enviar formulário:', error);
+          }
+        );
+    } else {
+      console.log('Por favor, preencha o formulário corretamente.');
+    }
   }
 }
